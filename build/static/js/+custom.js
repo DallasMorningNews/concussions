51bf7c73e3cd57b41ec3157b89f9f8b782fcc6d0
Kristi Walker <kwalker@tw80321nadnnd.ahc.belotechnologies.com>
2016-09-08 17:16:08 -0500

    adding images to bottom nav and transitions

--- build/static/js/+custom.js
+++ build/static/js/+custom.js
@@ -1,10 +1,37 @@
 $(document).ready(function() {
-	
+
 	//custom scripting goes here
+
+
+	$("#top-nav-wrapper").delay(800).slideDown();
+	setTimeout(function(){
+		$("#top-nav-1").delay(1500).addClass("active");
+	}, 1200);
+
+
+
+	// Makes bottom nav slide up and down
+
+	var slideBottom = $('#dfpAdPositionTop').offset().top;       // get initial position of the element
+
+
+	$(window).scroll(function() {                  // assign scroll event listener
+
+	    var currentScroll = $(window).scrollTop(); // get current position
+
+	    if (currentScroll >= slideBottom) {
+	        $("#bottom-nav-wrapper").slideDown(800);
+	    }
+		// else { $("#bottom-nav-wrapper").slideUp(800); }
+
+
+	});
+
+
 
 	// injecting current year into footer
 	// DO NOT DELETE
-	
+
 	var d = new Date();
 	var year = d.getFullYear();
 
@@ -17,4 +44,3 @@ $(document).ready(function() {
 
 
 });
-
--- public/js/scripts-bundle.js
+++ public/js/scripts-bundle.js
@@ -1,2 +1,2 @@
-$(document).ready(function(){var e=new Date,t=e.getFullYear();$(".copyright").text(t)});
+$(document).ready(function(){$("#top-nav-wrapper").delay(800).slideDown(),setTimeout(function(){$("#top-nav-1").delay(1500).addClass("active")},1200);var o=$("#dfpAdPositionTop").offset().top;$(window).scroll(function(){var t=$(window).scrollTop();t>=o&&$("#bottom-nav-wrapper").slideDown(800)});var t=new Date,e=t.getFullYear();$(".copyright").text(e)});
 //# sourceMappingURL=scripts-bundle.js.map
